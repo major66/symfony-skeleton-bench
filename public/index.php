836b0baccd9c9b73dd48f55012c719b863362674
major66 <yohann.payrot@epitech.eu>
2020-01-22 10:55:32 +0100

    Removed symfony usage.

--- public/index.php
+++ public/index.php
@@ -3,8 +3,11 @@
 use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 require dirname(__DIR__).'/config/bootstrap.php';
+
+return new Response('Hello World !');
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
@@ -19,6 +22,7 @@ if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ??
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
+
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
